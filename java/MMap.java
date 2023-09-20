3fd7df3f1c6d8b87e7e020a2d81d178a855bade2
dantinkakkar <dantin.kakkar@gmail.com>
2023-09-20 15:55:17 +0530

    [Java] Get file name from command line args

--- java/MMap.java
+++ java/MMap.java
@@ -1,5 +1,9 @@
 public class MMap {
     public static void main(String[] args) {
-        System.out.println("Placeholder code");
+        if (args.length == 0)
+            throw new RuntimeException("Expected 1 argument, filename missing");
+        if (args.length > 1)
+            throw new RuntimeException("Too many arguments");
+        final String filename = args[0];
     }
 }
