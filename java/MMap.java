494e2ee3001d7df0aa37748fb9303128755a548a
dantinkakkar <dantin.kakkar@gmail.com>
2023-09-20 16:04:12 +0530

    Use RandomAccessFile to open file via try-with-resources

--- java/MMap.java
+++ java/MMap.java
@@ -1,3 +1,9 @@
+import java.nio.channels.FileChannel;
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.io.IOException;
+
+
 public class MMap {
     public static void main(String[] args) {
         if (args.length == 0)
@@ -5,5 +11,13 @@ public class MMap {
         if (args.length > 1)
             throw new RuntimeException("Too many arguments");
         final String filename = args[0];
+        try (
+            final RandomAccessFile randomAccessFile = new RandomAccessFile(new File(filename), "r");
+            final FileChannel fileChannel = randomAccessFile.getChannel();
+        ) {
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
